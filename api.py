c5c32f696f0cad36b2361852f4a3e5601c868d4f
Tran Quoc Cuong <cuongtq15@cyberspace.vn>
2019-05-17 10:02:50 +0700

    first stable version

--- api.py
+++ api.py
@@ -2,7 +2,7 @@
     Receive a multipart data with field file
     Return a json file has 3 field 
         ("objects" ("class", "conf", "bbox" ("x", "y", "w", "h")), "code", "description")
-    curl -F "file=@image.png" 0.0.0.0:8000
+    curl -F "file=@image.png" 0.0.0.0:8080
 """
 import os
 import sys
@@ -23,11 +23,14 @@ app.config['TEMP_FOLDER'] = '/tmp/'
 app.secret_key = 'qcuong98 super secret key'
 app.config['SESSION_TYPE'] = 'filesystem'
 
+model_1 = None
+model_2 = None
+
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
 
-@app.route("/", methods = ["POST"])
-def upload_file():
+@app.route("/model_1", methods = ["POST"])
+def model_1():
     result = {
         "objects": [],
         "code": -1,
@@ -50,13 +53,80 @@ def upload_file():
             file.save(file_dir)
 
             image = cv2.imread(file_dir)
-            model_1 = model.get_model("cfg/coco.data", "cfg/yolov3.cfg", "weights/yolov3.weights")
-            return model.detect(model_1, image)[0][0], 200
 
-            # if os.path.exists(file_dir):
-            #     os.remove(file_dir)
-            # return jsonify(result), 200
-            
+            if os.path.exists(file_dir):
+                os.remove(file_dir)
+
+            objs = model.detect(model_1, image)
+            for obj in objs:
+                x, y, w, h = obj[2]
+                result["objects"].append({
+                    "class": obj[0].decode('UTF-8'),
+                    "conf": obj[1],
+                    "bbox": {
+                        "x": x - w/2,
+                        "y": y - h/2,
+                        "w": w,
+                        "h": h
+                    }
+                })
+            result["code"] = 0
+            result["description"] = "OK"
+            return jsonify(result), 200
+        else:
+            result["code"] = 2
+            result["description"] = "File extension isn't supported (bmp, png, jpg, jpeg, tif, tiff)"
+            return jsonify(result), 400
+
+    except Exception as e:
+            result["code"] = -1
+            result["description"] = "Error: %s" % e
+            return jsonify(result), 500
+
+@app.route("/model_2", methods = ["POST"])
+def model_2():
+    result = {
+        "objects": [],
+        "code": -1,
+        "description": "Processing"
+    }
+
+    try:
+        if 'file' not in request.files:
+            result["code"] = 1
+            result["description"] = "No 'file' field in request data"
+            return jsonify(result), 400
+        
+        file = request.files['file']
+        if allowed_file(file.filename):    
+            safe_name = secure_filename(file.filename)
+            _, temp_name = tempfile.mkstemp(
+                                    prefix = safe_name.split('.')[-2] + "_", 
+                                    suffix = "." + safe_name.split('.')[-1])
+            file_dir = os.path.join(app.config['TEMP_FOLDER'], temp_name)
+            file.save(file_dir)
+
+            image = cv2.imread(file_dir)
+
+            if os.path.exists(file_dir):
+                os.remove(file_dir)
+
+            objs = model.detect(model_2, image)
+            for obj in objs:
+                x, y, w, h = obj[2]
+                result["objects"].append({
+                    "class": obj[0].decode('UTF-8'),
+                    "conf": obj[1],
+                    "bbox": {
+                        "x": x - w/2,
+                        "y": y - h/2,
+                        "w": w,
+                        "h": h
+                    }
+                })
+            result["code"] = 0
+            result["description"] = "OK"
+            return jsonify(result), 200
         else:
             result["code"] = 2
             result["description"] = "File extension isn't supported (bmp, png, jpg, jpeg, tif, tiff)"
@@ -68,4 +138,6 @@ def upload_file():
             return jsonify(result), 500
 
 if (__name__ == '__main__'):
-    app.run(host = "0.0.0.0", port = 8000, debug = True)
+    model_1 = model.get_model("cfg/coco.data", "cfg/yolov3.cfg", "weights/yolov3.weights")
+    model_2 = model.get_model("cfg/yologo.data", "cfg/yologo.cfg", "weights/yologo.weights")
+    app.run(host = "0.0.0.0", port = 8080)
