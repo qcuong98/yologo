0c269d577194905a31635e69f8137d70f5f72941
Tran Quoc Cuong <cuongtq15@cyberspace.vn>
2019-05-23 12:20:20 +0700

    fix ignore

--- .debris/2019-05-23/model.py
+++
@@ -1,9 +0,0 @@
-import darknet
-
-def get_model(data_dir, cfg_dir, weights_dir):
-    net = darknet.load_net(cfg_dir.encode('utf-8'), weights_dir.encode('utf-8'), 0)
-    meta = darknet.load_meta(data_dir.encode('utf-8'))
-    return net, meta
-
-def detect(model, img_dir):
-    return darknet.detect(model[0], model[1], img_dir.encode('utf-8'))
